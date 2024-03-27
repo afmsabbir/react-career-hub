5ca75ae487b14ba5d0ac799be9d2f497d0d37e4a
afmsabbir <khanafmsabbir@gmail.com>
2024-03-28 00:48:00 +0600

    Set Nested Route And Navbar For CareerHub

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Oops...!</h1>
+            <Link className="btn" to={"/"}>Go Back</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
