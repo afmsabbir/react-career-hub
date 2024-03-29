fb6f8cd905266ceb00121567dc560f997942ee58
afmsabbir <khanafmsabbir@gmail.com>
2024-03-30 02:33:51 +0600

    Implement Job Filter And Deploy To Netlify And Surge

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -6,6 +6,22 @@ import Job from "../Job/Job";
 const AppliedJobs = () => {
   const jobs = useLoaderData();
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+  const handleFilterJobs = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJob = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJob);
+    }
+  };
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
     if (jobs.length > 0) {
@@ -18,25 +34,26 @@ const AppliedJobs = () => {
         }
       }
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
     }
-  }, []);
+  }, [jobs]);
   return (
     <div>
       <details className="dropdown ">
         <summary className="m-1 btn">Filter By</summary>
         <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-          <li>
+          <li onClick={() => handleFilterJobs("all")}>
             <a>All</a>
           </li>
-          <li>
+          <li onClick={() => handleFilterJobs("remote")}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={() => handleFilterJobs("onsite")}>
             <a>Onsite</a>
           </li>
         </ul>
       </details>
-      {appliedJobs.map((job) => (
+      {displayJobs.map((job) => (
         <Job key={job.id} job={job}></Job>
       ))}
     </div>
